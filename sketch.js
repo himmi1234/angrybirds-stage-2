7d026540f4e58aa67e7718a74658fc735ad32347
himmi1234 <himmigupta1991@gmail.com>
2020-06-15 13:36:43 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = nnew Ground(200,200,1200,30);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -57,6 +58,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    platform.display();
 
     bird.display();
 }
